685bc521ac5c014cc126405773f866f5557ed388
Pedrodesousa <pedro_de_sousa@outlook.com>
2023-02-28 13:44:41 +0100

    fix package error

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ Encore
    * Each entry will result in one JavaScript file (e.g. app.js)
    * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
    */
-  .addEntry("app", "./assets/index.js")
+  .addEntry("app", "./assets")
 
   // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
   .splitEntryChunks()
